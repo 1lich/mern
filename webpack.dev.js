455462322491603355bf089fb8b5853fcd75fd46
holykim <holykim@mail.ru>
2024-01-14 18:21:52 +0300

    add scripts

---
+++ webpack.dev.js
@@ -0,0 +1,30 @@
+import { merge } from "webpack-merge"
+
+import common from "./webpack.common.js"
+
+const PORT = 8000
+
+const config = {
+  mode: "development",
+  optimization: {
+    usedExports: true,
+  },
+  devServer: {
+    hot: true,
+    historyApiFallback: true,
+    port: `${PORT}`,
+    host: "192.168.31.130",
+    client: {
+      overlay: {
+        warnings: false,
+        errors: true,
+      },
+    },
+    proxy: {
+      context: ["/api"],
+      target: `http://localhost:${PORT}`,
+    },
+  },
+}
+
+export default merge(common, config)
---
+++ webpack.prod.js
@@ -0,0 +1,38 @@
+import { merge } from "webpack-merge"
+
+import common from "./webpack.common.js"
+import TerserPlugin from "terser-webpack-plugin"
+import CssMinimizerPlugin from "css-minimizer-webpack-plugin"
+
+const config = {
+  mode: "production",
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
+        },
+      }),
+      new CssMinimizerPlugin({
+        exclude: /node_modules/,
+        parallel: true,
+        minimizerOptions: {
+          preset: [
+            "default",
+            {
+              discardCommnets: {
+                removeAll: true,
+              },
+            },
+          ],
+        },
+      }),
+    ],
+  },
+}
+
+export default (common, config)
